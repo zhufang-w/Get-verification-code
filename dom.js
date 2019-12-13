330ec8b872261aa6fd01b322014c015153d3a27b
zhufang <zhufang_w@163.com>
2019-12-13 22:39:02 +0800

    first

---
+++ dom.js
@@ -0,0 +1,9 @@
+﻿function createRndCode(){
+	var arr=['1','2','3','4','5','a','b','c','d','e'];
+	var yzm="";
+	for (var i = 0; i < 3; i++) {
+	var	xb=Math.floor(Math.random()*(arr.length-1));
+		yzm+=arr[xb];	
+	}
+	document.getElementById("code").innerHTML=yzm;
+}
